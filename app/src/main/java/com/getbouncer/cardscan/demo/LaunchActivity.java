6c805e6e2a846c081117479ec9a653e6cddf14d3
Adam Wushensky <adam@getbouncer.com>
2020-02-19 12:32:04 -0800

    Better error handling

--- app/src/main/java/com/getbouncer/cardscan/demo/LaunchActivity.java
+++ app/src/main/java/com/getbouncer/cardscan/demo/LaunchActivity.java
@@ -29,18 +29,55 @@ public class LaunchActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (CardScanActivity.isScanResult(requestCode)) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
             if (resultCode == Activity.RESULT_OK && data != null) {
-                ScanResult scanResult = CardScanActivity.getResult(data);
+                ScanResult scanResult = CardScanActivity.getScannedCard(data);
                 if (scanResult != null) {
-                    builder.setMessage(scanResult.getPan());
+                    handleCardScanned(scanResult);
                 } else {
-                    builder.setMessage(R.string.null_result);
+                    handleCardScanCancelled();
                 }
             } else if (resultCode == Activity.RESULT_CANCELED) {
-                builder.setMessage(R.string.scan_canceled);
+                int cancelledReason = data.getIntExtra(CardScanActivity.RESULT_CANCELED_REASON, -1);
+                if (cancelledReason == CardScanActivity.CANCELED_REASON_ENTER_MANUALLY) {
+                    handleEnterCardManually();
+                } else if (cancelledReason == CardScanActivity.CANCELED_REASON_CAMERA_ERROR) {
+                    handleCameraError();
+                } else if (cancelledReason == CardScanActivity.CANCELED_REASON_USER) {
+                    handleCardScanCancelled();
+                } else {
+                    handleCardScanCancelledUnknown();
+                }
             }
-            builder.show();
         }
+    }
+
+    private void handleCardScanned(ScanResult scanResult) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(scanResult.getPan());
+        builder.show();
+    }
+
+    private void handleCardScanCancelled() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.scan_canceled);
+        builder.show();
+    }
+
+    private void handleEnterCardManually() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.enter_manually);
+        builder.show();
+    }
+
+    private void handleCameraError() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.camera_error);
+        builder.show();
+    }
+
+    private void handleCardScanCancelledUnknown() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.unknown_reason);
+        builder.show();
     }
 }
